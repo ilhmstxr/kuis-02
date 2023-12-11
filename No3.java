d3835f0c22919de45369207669fac0ba604031c5
Claudioanjay <145308591+Claudioanjay@users.noreply.github.com>
2023-12-11 08:59:25 +0700

    Add files via upload

---
+++ No3.java
@@ -0,0 +1,19 @@
+package kuis2dio;
+public class No3 {
+    public static void main(String[] args) {
+    String[][] namadanharga = {
+            {"Kursi"   ,"50000"},
+            {"Meja"    ,"200000"},
+            {"Pensil", "3000"},
+            {"Baju", "50000"},
+            {"Botol", "65000"}
+        };
+        System.out.println("Nama Barang |  Harga Barang");
+    for(int i=0;i<=4;i++){
+        
+        System.out.print(namadanharga[i][0]+"           ");
+        System.out.println("Rp "+namadanharga[i][1]+"");
+    }      
+  }
+}
+
