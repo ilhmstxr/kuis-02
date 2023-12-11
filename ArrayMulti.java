4769de89577fac0e9b241afc1a96d664c65b9e36
Claudioanjay <145308591+Claudioanjay@users.noreply.github.com>
2023-12-11 09:19:43 +0700

    Add files via upload

---
+++ ArrayMulti.java
@@ -0,0 +1,22 @@
+package kuis2dio;
+public class ArrayMulti {
+    public static void main(String[] args) {
+    String[][] namadanharga = {
+            {"Kursi"   ,"50000"},
+            {"Meja"    ,"200000"},
+            {"Pensil", "3000"},
+            {"Baju", "50000"},
+            {"Botol", "65000"},
+            {"Buku", "23000"},
+            {"Kabel", "9000"},
+            {"Bolpoin", "4000"},
+        };
+        System.out.println("Nama Barang |  Harga Barang");
+    for(int i=0;i<=7;i++){
+        
+        System.out.print(namadanharga[i][0]+"           ");
+        System.out.println("Rp "+namadanharga[i][1]+"");
+    }      
+  }
+}
+
